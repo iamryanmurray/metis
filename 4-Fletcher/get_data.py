0cd4cfc9cb658d0cd41aaf254794d9b24a61a8c0
Ryan Murray <ryan@murray.li>
2018-05-30 14:10:42 -0700

    lda/svd/kmeans

--- 4-Fletcher/get_data.py
+++ 4-Fletcher/get_data.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 
 def import_from_mongo():
+	#loads data from mongo into dataframe and returns a list of space 
+	#separated titles and a list of full transcripts
 	client = MongoClient()
 	ted = client.ted_database
 	df = pd.DataFrame(list(ted.ted_transcripts.find()))
