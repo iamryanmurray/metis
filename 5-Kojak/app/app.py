97621bd7b9f7ed88cd27df877050926aa6425823
Ryan Murray <ryan@murray.li>
2018-06-14 16:20:54 -0700

    app

--- 5-Kojak/app/app.py
+++ 5-Kojak/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from flask_uploads import UploadSet, configure_uploads,IMAGES
 from scipy.misc import imsave, imread, imresize
 from keras.preprocessing import image
@@ -18,6 +18,7 @@ from load import *
 app = Flask(__name__)
 
 global model ,idx2class,graph
+
 
 model,idx2class,graph = init()
 
@@ -46,10 +47,37 @@ def upload():
 		img_data = np.expand_dims(img_data,axis=0)
 		img_data = preprocess_input(img_data)
 		with graph.as_default():
-
+			global pred_class
 			pred_class = idx2class[model.predict_classes(img_data)[0]]
+		
 
 			return render_template("index2.html",cl = pred_class)
+
+@app.route('/success',methods=['GET','POST'])
+def correct_prediction():
+	if request.method == 'POST':
+		os.rename('./output.png', './images/'+ pred_class +'_{0}.png'.format(np.random.randint(100000)))
+		return render_template('success.html')
+	return render_template('success.html')
+
+@app.route('/failure',methods=['GET','POST'])
+def incorrect_prediction():
+	if request.method == 'POST':
+		return render_template('failure.html')
+
+
+
+@app.route('/label',methods=['GET','POST'])
+def label_image():
+	make = request.form['make'].lower()
+	model = request.form['model'].lower()
+	os.rename('./output.png','./images/'+make+'_'+model+'_{0}.png'.format(np.random.randint(1000000)))
+	return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route("/<string:page_name>")
@@ -60,3 +88,4 @@ if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 5000))
 
 	app.run(host='0.0.0.0',port = port)
+	app.run(debug=True)
